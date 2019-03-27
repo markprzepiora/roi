6157a3ff5023633fd45068af4c01b98b3a65247d
Mark Przepiora <mark.przepiora@gmail.com>
2019-03-27 17:57:29 -0600

    Add test for Roi.boolean.or_nil

--- spec/roi/schemas/boolean_schema_spec.rb
+++ spec/roi/schemas/boolean_schema_spec.rb
@@ -3,4 +3,8 @@ describe "Roi.boolean" do
     schema: 'Roi.boolean',
     passing_values: [ true, false ],
     failing_values: [ nil, 'true', 'false', 0, 1, '0', '1', 'on', 'off' ]
+
+  include_examples "passing and failing values",
+    schema: 'Roi.boolean.or_nil',
+    passing_values: [ true, false, nil ]
 end
