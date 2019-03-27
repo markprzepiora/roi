7879c28cb775cc55a15447552559ffcfd69e212a
Mark Przepiora <mark.przepiora@gmail.com>
2019-03-27 08:28:46 -0600

    group tests

--- spec/roi_spec.rb
+++ spec/roi_spec.rb
@@ -3,58 +3,62 @@ describe Roi do
     Roi::VERSION.should_not be_nil
   end
 
-  it "validates a simple string schema (pass)" do
-    value = "a string"
-    schema = Roi.string
-    result = schema.validate(value)
+  describe ".string" do
+    it "validates a simple string schema (pass)" do
+      value = "a string"
+      schema = Roi.string
+      result = schema.validate(value)
 
-    result.should be_ok
-    result.value.should == value
+      result.should be_ok
+      result.value.should == value
+    end
+
+    it "validates a simple string schema (fail)" do
+      value = nil
+      schema = Roi.string
+      result = schema.validate(value)
+
+      result.should_not be_ok
+    end
   end
 
-  it "validates a simple string schema (fail)" do
-    value = nil
-    schema = Roi.string
-    result = schema.validate(value)
+  describe ".object" do
+    it "successfully validates a trivial object schema and returns the input" do
+      hash = {}
+      schema = Roi.object
+      result = schema.validate(hash)
 
-    result.should_not be_ok
-  end
+      result.should be_ok
+      result.value.should == hash
+    end
 
-  it "successfully validates a trivial object schema and returns the input" do
-    hash = {}
-    schema = Roi.object
-    result = schema.validate(hash)
+    it "rejects a non-Hash when the schema is of type object" do
+      value = "a string"
+      schema = Roi.object
+      result = schema.validate(value)
 
-    result.should be_ok
-    result.value.should == hash
-  end
+      result.should_not be_ok
+    end
 
-  it "rejects a non-Hash when the schema is of type object" do
-    value = "a string"
-    schema = Roi.object
-    result = schema.validate(value)
+    it "validates an object schema with a key (pass)" do
+      value = { name: "Mark" }
+      schema = Roi.object.keys({
+        name: Roi.string
+      })
+      result = schema.validate(value)
 
-    result.should_not be_ok
-  end
+      result.should be_ok
+      result.value.should == value
+    end
 
-  it "validates an object schema with a key (pass)" do
-    value = { name: "Mark" }
-    schema = Roi.object.keys({
-      name: Roi.string
-    })
-    result = schema.validate(value)
+    it "validates an object schema with a key (fail)" do
+      value = { name: 123 }
+      schema = Roi.object.keys({
+        name: Roi.string
+      })
+      result = schema.validate(value)
 
-    result.should be_ok
-    result.value.should == value
-  end
-
-  it "validates an object schema with a key (fail)" do
-    value = { name: 123 }
-    schema = Roi.object.keys({
-      name: Roi.string
-    })
-    result = schema.validate(value)
-
-    result.should_not be_ok
+      result.should_not be_ok
+    end
   end
 end
