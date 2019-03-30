d9c5c6ec36ba1da8dc1caaab59ea01e161e5b13b
Mark Przepiora <mark.przepiora@gmail.com>
2019-03-29 23:53:59 -0600

    Remove dead code

--- lib/roi/test.rb
+++ lib/roi/test.rb
@@ -9,28 +9,5 @@ class Roi::Test
     @proc = proc
   end
 
-  def in_context(schema, context)
-    TestContext.new(schema, self, context)
-  end
-
   def_delegator :@proc, :call
-
-  class TestContext < SimpleDelegator
-    def initialize(schema_instance, test, context)
-      @__test__ = test
-      @__context__ = context
-      schema_instance.instance_variables.each do |name|
-        instance_variable_set(name, schema_instance.instance_variable_get(name))
-      end
-      super(schema_instance)
-    end
-
-    def call(*args)
-      instance_exec(*args, &@__test__.proc)
-    end
-
-    def error(message)
-      @__context__.error(validator_name: @__test__.name, message: message)
-    end
-  end
 end
