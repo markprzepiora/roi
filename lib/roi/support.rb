e8e697c11e51dd93f3a5c147613fd0949c7a140a
Mark Przepiora <mark.przepiora@gmail.com>
2019-03-30 12:25:08 -0600

    Polyfill Hash#slice

---
+++ lib/roi/support.rb
@@ -0,0 +1,14 @@
+require 'roi'
+
+module Roi::Support
+  refine Hash do
+    if !Hash.instance_methods.include?(:slice)
+      def slice(*keys)
+        keys = keys.map! { |key| convert_key(key) } if respond_to?(:convert_key)
+        hash = self.class.new
+        keys.each { |k| hash[k] = self[k] if has_key?(k) }
+        hash
+      end
+    end
+  end
+end
