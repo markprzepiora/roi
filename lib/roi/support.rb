d18553b7c7e3607489d1e526c88c02c0a77e2991
Mark Przepiora <mark.przepiora@gmail.com>
2019-04-01 09:41:44 -0600

    Add Ruby 2.3 #dig polyfills

--- lib/roi/support.rb
+++ lib/roi/support.rb
@@ -10,12 +10,89 @@ module Roi::Support
         hash
       end
     end
+
+    if !Hash.instance_methods.include?(:dig)
+      def dig(head, *rest)
+        [head, *rest].reduce(self) do |value, accessor|
+          next_value =
+            case value
+            when ::Array
+              value.at(accessor)
+            when ::Hash
+              value[accessor]
+            when ::Struct
+              value[accessor] if value.members.include?(accessor)
+            else
+              begin
+                break value.dig(*rest)
+              rescue NoMethodError
+                raise TypeError, "#{value.class} does not have a #dig method"
+              end
+            end
+
+          break nil if next_value.nil?
+          next_value
+        end
+      end
+    end
   end
 
   refine Array do
     if !Array.instance_methods.include?(:to_h)
       def to_h
         Hash[*flatten(1)]
+      end
+    end
+
+    if !Array.instance_methods.include?(:dig)
+      def dig(head, *rest)
+        [head, *rest].reduce(self) do |value, accessor|
+          next_value =
+            case value
+            when ::Array
+              value.at(accessor)
+            when ::Hash
+              value[accessor]
+            when ::Struct
+              value[accessor] if value.members.include?(accessor)
+            else
+              begin
+                break value.dig(*rest)
+              rescue NoMethodError
+                raise TypeError, "#{value.class} does not have a #dig method"
+              end
+            end
+
+          break nil if next_value.nil?
+          next_value
+        end
+      end
+    end
+  end
+
+  refine Struct do
+    if !Struct.instance_methods.include?(:dig)
+      def dig(head, *rest)
+        [head, *rest].reduce(self) do |value, accessor|
+          next_value =
+            case value
+            when ::Array
+              value.at(accessor)
+            when ::Hash
+              value[accessor]
+            when ::Struct
+              value[accessor] if value.members.include?(accessor)
+            else
+              begin
+                break value.dig(*rest)
+              rescue NoMethodError
+                raise TypeError, "#{value.class} does not have a #dig method"
+              end
+            end
+
+          break nil if next_value.nil?
+          next_value
+        end
       end
     end
   end
