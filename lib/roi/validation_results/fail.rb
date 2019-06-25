434583e49341985a782d2ffe4fe76334b8af06b7
Mark Przepiora <mark.przepiora@gmail.com>
2019-06-25 03:09:11 -0600

    Type check Fail and Pass

--- lib/roi/validation_results/fail.rb
+++ lib/roi/validation_results/fail.rb
@@ -1,15 +1,21 @@
 # typed: true
+
 require 'roi/validation_results'
+require 'sorbet-runtime'
 
 class Roi::ValidationResults::Fail
+  extend T::Sig
+
   # @return [Array<Roi::ValidationError>]
   attr_reader :errors
 
+  sig{ params(errors: T::Array[Roi::ValidationError] ).void }
   def initialize(errors = [])
     @errors = errors
   end
 
   # @return false
+  sig{ returns(T::Boolean) }
   def ok?
     false
   end
--- lib/roi/validation_results/pass.rb
+++ lib/roi/validation_results/pass.rb
@@ -1,7 +1,11 @@
 # typed: true
+
 require 'roi/validation_results'
+require 'sorbet-runtime'
 
 class Roi::ValidationResults::Pass
+  extend T::Sig
+
   # The object successfully validated and returned by the schema.
   #
   # @return [Object]
@@ -11,12 +15,14 @@ class Roi::ValidationResults::Pass
   attr_reader :pass_early
   alias_method :pass_early?, :pass_early
 
+  sig{ params(value: T.untyped, pass_early: T::Boolean).void }
   def initialize(value, pass_early: false)
     @value = value
     @pass_early = pass_early
   end
 
   # @return true
+  sig{ returns(T::Boolean) }
   def ok?
     true
   end
