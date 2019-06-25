677c5e5529a0926dc88dc5d0cd15fe4e7e1d6c04
Mark Przepiora <mark.przepiora@gmail.com>
2019-06-25 02:21:20 -0600

    Add first signatures

--- lib/roi/test.rb
+++ lib/roi/test.rb
@@ -1,16 +1,23 @@
 # typed: true
 
 require 'roi'
+require 'sorbet-runtime'
 
 class Roi::Test
+  extend T::Sig
   extend Forwardable
   attr_reader :name, :method_name
 
+  sig{ params(name: String, method_name: Symbol).void }
   def initialize(name, method_name)
     @name = name
     @method_name = method_name
   end
 
+  sig{
+    params(schema: Roi::Schemas::BaseSchema, value: T.untyped, context: Roi::ValidationContext).
+    returns(T.any(Roi::ValidationResults::Pass, Roi::ValidationResults::Fail, T.untyped))
+  }
   def run(schema, value, context)
     schema.send(method_name, value, context)
   end
